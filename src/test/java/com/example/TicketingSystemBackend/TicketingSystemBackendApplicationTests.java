4851a07c6420e73a9f1e64c159220a17c226b5ff
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2023-09-04 23:14:05 +0700

    Starter

---
+++ src/test/java/com/example/TicketingSystemBackend/TicketingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicketingSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
