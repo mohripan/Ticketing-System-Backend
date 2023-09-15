cabd25024b4cff9abceeabba04c1aeb7349190fe
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2023-09-15 23:30:53 +0700

    Add Ticket Service

--- src/main/java/com/example/TicketingSystemBackend/service/TicketService.java
+++ src/main/java/com/example/TicketingSystemBackend/service/TicketService.java
@@ -1,4 +1,28 @@
 package com.example.TicketingSystemBackend.service;
 
+import com.example.TicketingSystemBackend.model.Department;
+import com.example.TicketingSystemBackend.model.Ticket;
+import com.example.TicketingSystemBackend.model.User;
+import com.example.TicketingSystemBackend.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public List<Ticket> getTicketsByUser(User user) {
+        return ticketRepository.findByUser(user);
+    }
+
+    public List<Ticket> getTicketsByAssignedTo(Integer assignedTo) {
+        return ticketRepository.findByAssignedTo(assignedTo);
+    }
+
+    public List<Ticket> getTicketsByDepartment(Department department) {
+        return ticketRepository.findByTicketTagDepartment(department);
+    }
 }
