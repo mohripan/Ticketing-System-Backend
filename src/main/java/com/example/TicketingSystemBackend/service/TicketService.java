e98c8a81e710eb8e7e8ee8b4561c460f45fda974
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2023-09-15 23:32:54 +0700

    Add Filtering

--- src/main/java/com/example/TicketingSystemBackend/service/TicketService.java
+++ src/main/java/com/example/TicketingSystemBackend/service/TicketService.java
@@ -2,11 +2,13 @@ package com.example.TicketingSystemBackend.service;
 
 import com.example.TicketingSystemBackend.model.Department;
 import com.example.TicketingSystemBackend.model.Ticket;
+import com.example.TicketingSystemBackend.model.TicketSeverity;
 import com.example.TicketingSystemBackend.model.User;
 import com.example.TicketingSystemBackend.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -24,5 +26,17 @@ public class TicketService {
 
     public List<Ticket> getTicketsByDepartment(Department department) {
         return ticketRepository.findByTicketTagDepartment(department);
+    }
+
+    public List<Ticket> getTicketsByStatus(String ticketStatus) {
+        return ticketRepository.findByTicketStatus(ticketStatus);
+    }
+
+    public List<Ticket> getTicketsBySeverity(TicketSeverity ticketSeverity) {
+        return ticketRepository.findByTicketSeverity(ticketSeverity);
+    }
+
+    public List<Ticket> getTicketsByDateRange(LocalDateTime startDate, LocalDateTime endDate) {
+        return ticketRepository.findByCreatedDateBetween(startDate, endDate);
     }
 }
