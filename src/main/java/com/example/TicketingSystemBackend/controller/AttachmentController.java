57c1dd703e92f7fa2c98dda614311362cd4b97fc
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2023-10-16 20:18:49 +0700

    File

--- src/main/java/com/example/TicketingSystemBackend/controller/AttachmentController.java
+++ src/main/java/com/example/TicketingSystemBackend/controller/AttachmentController.java
@@ -1,14 +1,20 @@
 package com.example.TicketingSystemBackend.controller;
 
+import com.example.TicketingSystemBackend.message.ResponseMessage;
 import com.example.TicketingSystemBackend.model.Attachment;
 import com.example.TicketingSystemBackend.service.AttachmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/attachments")
@@ -20,7 +26,7 @@ public class AttachmentController {
     public ResponseEntity<Attachment> uploadAttachment(@PathVariable Integer ticketId,
                                                        @RequestParam("file") MultipartFile file) {
         try {
-            Attachment attachment = attachmentService.saveAttachment(file, ticketId);
+            Attachment attachment = attachmentService.save(file, ticketId);
             return new ResponseEntity<>(attachment, HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
@@ -29,10 +35,54 @@ public class AttachmentController {
         }
     }
 
-    @GetMapping("/{attachmentId}")
-    public ResponseEntity<Attachment> getAttachment(@PathVariable Integer attachmentId) {
-        Optional<Attachment> attachment = attachmentService.findById(attachmentId);
-        return attachment.map(response -> ResponseEntity.ok().body(response))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    @PostMapping("/upload/multiple/{ticketId}")
+    public ResponseEntity<List<Attachment>> uploadMultipleAttachments(@PathVariable Integer ticketId,
+                                                                      @RequestParam("files") MultipartFile[] files) {
+        try {
+            List<Attachment> attachments = new ArrayList<>();
+            for (MultipartFile file : files) {
+                Attachment attachment = attachmentService.save(file, ticketId);
+                attachments.add(attachment);
+            }
+            return new ResponseEntity<>(attachments, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = attachmentService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+    @GetMapping("/list/{ticketId}")
+    public ResponseEntity<List<Attachment>> listAttachments(@PathVariable Integer ticketId) {
+        List<Attachment> attachments = attachmentService.loadAllByTicket(ticketId);
+        return new ResponseEntity<>(attachments, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{attachmentId}")
+    public ResponseEntity<?> deleteAttachment(@PathVariable Integer attachmentId) {
+        try {
+            attachmentService.delete(attachmentId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/all/{ticketId}")
+    public ResponseEntity<?> deleteAllAttachments(@PathVariable Integer ticketId) {
+        try {
+            attachmentService.deleteAllByTicket(ticketId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/example/TicketingSystemBackend/service/AttachmentService.java
+++ src/main/java/com/example/TicketingSystemBackend/service/AttachmentService.java
@@ -4,16 +4,22 @@ import com.example.TicketingSystemBackend.model.Attachment;
 import com.example.TicketingSystemBackend.model.Ticket;
 import com.example.TicketingSystemBackend.repository.AttachmentRepository;
 import com.example.TicketingSystemBackend.repository.TicketRepository;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AttachmentService {
@@ -23,33 +29,77 @@ public class AttachmentService {
     @Autowired
     private TicketRepository ticketRepository;
 
-    public Attachment saveAttachment(MultipartFile file, Integer ticketId) throws IOException {
-        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(() -> new RuntimeException("Ticket not found"));
-        Attachment attachment = new Attachment();
+    private final Path root = Paths.get("C:\\Users\\mohri\\OneDrive\\Documents\\file_test");
 
-        // Inside the project's folder:
-        // String directory = "test_file";
+    public Attachment save(MultipartFile file, Integer ticketId) {
+        try {
+            Ticket ticket = ticketRepository.findById(ticketId)
+                    .orElseThrow(() -> new RuntimeException("Ticket not found"));
 
-        // Outside the project's folder:
-        String directory = "C:\\Users\\mohri\\OneDrive\\Documents\\file_test";
+            Path filePath = root.resolve(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), filePath);
 
-        Path path = Paths.get(directory);
-        if (!Files.exists(path)) {
-            Files.createDirectories(path);
+            Attachment attachment = new Attachment();
+            attachment.setTicket(ticket);
+            attachment.setAttachmentPath(filePath.toString());
+            attachment.setFileName(file.getOriginalFilename());
+            attachment.setFileType(file.getContentType());
+            attachment.setFileSize(file.getSize());
+            attachment.setUploadDate(LocalDateTime.now());
+
+            return attachmentRepository.save(attachment);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
         }
+    }
 
-        String originalFileName = file.getOriginalFilename();
-        Path filePath = path.resolve(originalFileName);
-        file.transferTo(filePath.toFile());
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
 
-        attachment.setTicket(ticket);
-        attachment.setAttachmentPath(filePath.toString());
-        attachment.setFileName(file.getOriginalFilename());
-        attachment.setFileType(file.getContentType());
-        attachment.setFileSize(file.getSize());
-        attachment.setUploadDate(LocalDateTime.now());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
 
-        return attachmentRepository.save(attachment);
+    public List<Attachment> loadAllByTicket(Integer ticketId) {
+        return attachmentRepository.findAll().stream()
+                .filter(attachment -> attachment.getTicket().getTicketID().equals(ticketId))
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Integer attachmentId) {
+        Attachment attachment = attachmentRepository.findById(attachmentId)
+                .orElseThrow(() -> new RuntimeException("Attachment not found"));
+
+        try {
+            Files.delete(Paths.get(attachment.getAttachmentPath()));
+            attachmentRepository.deleteById(attachmentId);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not delete the file. Error: " + e.getMessage());
+        }
+    }
+
+    public void deleteAllByTicket(Integer ticketId) {
+        List<Attachment> attachments = attachmentRepository.findAll().stream()
+                .filter(attachment -> attachment.getTicket().getTicketID().equals(ticketId))
+                .collect(Collectors.toList());
+
+        for (Attachment attachment : attachments) {
+            try {
+                Files.delete(Paths.get(attachment.getAttachmentPath()));
+                attachmentRepository.delete(attachment);
+            } catch (IOException e) {
+                throw new RuntimeException("Could not delete the file. Error: " + e.getMessage());
+            }
+        }
     }
 
     public Optional<Attachment> findById(Integer id) {
